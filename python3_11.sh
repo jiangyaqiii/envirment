9b0172215e08c92ad65d853b22d4fad91538a116
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-12-05 18:58:14 +0800

    Update python3_11.sh

--- python3_11.sh
+++ python3_11.sh
@@ -4,9 +4,9 @@ echo -e "\n" |sudo add-apt-repository ppa:deadsnakes/ppa
 ###########
 ##安装python3.10
 sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
-wget https://www.python.org/ftp/python/3.9.8/Python-3.9.8.tgz 
-tar -xf Python-3.9.8.tgz
-cd Python-3.9.8
+wget https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tgz 
+tar -xf Python-3.11.0.tgz
+cd Python-3.11.0
 ./configure --enable-optimizations
 make -j `nproc`
 sudo make altinstall
