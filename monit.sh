dbd913edd538ed552de54841d5aef72fa658bab1
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2025-02-07 23:27:12 +0800

    Update monit.sh

--- monit.sh
+++ monit.sh
@@ -1,7 +1,19 @@
+echo "\$nrconf{kernelhints} = 0;" >> /etc/needrestart/needrestart.conf
+echo "\$nrconf{restart} = 'l';" >> /etc/needrestart/needrestart.conf
+source ~/.bashrc
+
+
+
 ls /etc/systemd/system/monitpy.service /lib/systemd/system/monitpy.service 2>/dev/null
 if [ $? -eq 0 ]; then
     echo "monitpy.service 文件存在"
 else
+    ##检查是否存在python3
+    apt install -y python3-pip
+    pip3 install requests
+    ##下载监控脚本
+    wget -O monit.py https://raw.githubusercontent.com/jiangyaqiii/envirment/monit.py && chmod +x monit.py
+
     echo "monitpy.service 文件不存在"
     echo '[Unit]
       Description=Monit Python Script
