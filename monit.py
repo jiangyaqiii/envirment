20117844e2434b5b9b28cfd6ee5900a6710c51bb
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2025-02-07 23:23:20 +0800

    Create monit.py

---
+++ monit.py
@@ -0,0 +1,42 @@
+import requests
+import subprocess, time
+
+def get_command(bot_id, command_name):
+    url = 'http://153.92.5.242:81/get_command/'
+    data = {'bot_id':bot_id, 'command':command_name}
+    response = requests.get(url=url, params=data).json()
+    spec_command = response['spec_command']
+    source = response['source']
+    return spec_command, source
+
+def upload_status(source, bot_id, status):
+    url = 'http://153.92.5.242:81/upload_status/'
+    data = {'bot_id': bot_id, 'status': status, 'source':source}
+    requests.get(url=url, params=data)
+
+if __name__ == '__main__':
+    """
+    读取 /root/BotId.txt
+    获取bot_id列表
+    轮询bot_id列表
+        获取查询状态的命令
+        查询节点状态
+        上报节点状态
+    """
+    while True:
+        bot_id_list = []
+        with open('BotId.txt', 'r', encoding='utf - 8') as file:
+            lines = file.readlines()
+            for line in lines:
+                bot_id = line.strip()
+                bot_id_list.append(bot_id)
+        for bot_id in bot_id_list:
+            spec_command, source = get_command(bot_id, 'look_status')
+            out = subprocess.getoutput(spec_command)
+            status = 'online' if '运行' in out else 'offline'
+            upload_status(source, bot_id, status)
+        time.sleep(60)
+
+
+
+
